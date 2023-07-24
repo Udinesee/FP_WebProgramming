537f8b61eb7e5f847b6f0bf4cd6c3ae7a891247b
Udinesee <akhmadkhoirudin85@gmail.com>
2023-07-24 15:57:25 +0700

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Toggle class active
-const navbarNav = document.querySelector(".navbar-nav");
+const navbarNav = document.querySelector('.navbar-nav');
 
 // ketika  hamburger di klik
-document.querySelector("#hamburger-menu").oneclick = () => {
-  navbarNav.classList.toggle("active");
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
 };
 // klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 
-document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
+document.addEventListener('click', function(e) {
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
   }
 });
+ 
